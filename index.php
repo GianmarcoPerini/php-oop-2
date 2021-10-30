19ded29a7b1163c5aa650d92a6c66ae7b4ec31a7
GianmarcoPerini <g.marco.perini@gmail.com>
2021-10-30 16:08:59 +0200

    conclusion

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
     class Product{
-        protected $name;
-        protected $brand;
-        protected $price;
-        protected $category;
+        public $name;
+        public $brand;
+        public $price;
+        public $category;
 
         public function __construct($name, $brand, $price, $category){
             $this->name = $name;
@@ -35,17 +35,26 @@
         protected $surname;
         protected $adress;
         protected $premium;
+        protected $total;
 
-        public function __construct($id, $name, $surname, $adress, $premium = false){
+        public function __construct($id, $name, $surname, $adress, $premium = false, $total = 0){
             $this->id = $id;
             $this->name = $name;
             $this->surname = $surname;
             $this->adress = $adress;
             $this->premium = $premium;
+            $this->total = $total;
         }
 
         public function setPremium($bool){
-            return $this->premium = $bool;
+            $this->premium = $bool;
+        }
+
+        public function updateTotal($total){
+            if($this->premium == true){
+                return $this->total = $total * 0.85;
+            }
+            return $this->total = $total;
         }
     }
 
@@ -54,43 +63,45 @@
         protected $cardCode;
         protected $expDate;
         protected $ccv;
-
-        public function __construct($cardID, $cardCode, $expDate, $ccv){
-
-            $this->cardID = $cardID;
-            $this->cardCode = $cardCode;
-            $this->expDate = $expDate;
-            $this->ccv = $ccv;
-        }
     }
 
     class Cart{
-        public $products = [];
+        public $products;
+        public $cartPrice;
 
-        public function setProducts($product){
-            return $this->products[] = $product;
+        public function setProducts(...$product){
+            $this->products = $product;
         }
 
-        public function checkPremium($product, $user){
+        public function setTotal($user){
+            return $user->updateTotal($this->cartPrice);
+        }
 
-            $productPrice = $product->getPrice();
+        public function getTotalPrice(){
+            foreach( $this->products as $prod){
+                $this->cartPrice += $prod->price;
+            }
+        }
 
-            if( $productPrice > 100 ){
+        public function checkPremium($user){
+            if( $this->cartPrice > 100 ){
                 return $user->setPremium(true);
             }
         }
     }
 
     $leo = new User(551, 'Leonardo', 'Pascal', 'via Zianigo');
-    $telefono = new Product('xl-34-gc', 'Nokia', 150, 'Elettronic');
+    $telefono1 = new Product('xl-34-gc', 'Nokia', 20, 'Elettronic');
+    $telefono2 = new Product('xl-3s4-gc', 'Nokia', 10, 'Elettronic');
+    $telefono3 = new Product('xl-3f4-gc', 'Nokia', 600, 'Elettronic');
     $leoCard = new Card(785, 654862154895,'12/5/2026', 556);
     $cart = new Cart();
-    $cart->setProducts($telefono);
-    $cart->checkPremium($telefono, $leo);
+    $cart->setProducts($telefono1, $telefono2, $telefono3);
+    $cart->getTotalPrice();
+    $cart->checkPremium($leo);
+    $cart->setTotal($leo);
     
 
-    var_dump($leoCard);
-    var_dump($telefono);
     var_dump($leo);
 
 ?>
